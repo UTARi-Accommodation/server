48a8e9a57e87ff64183c0a89ddd9ddb6124ec27a
GervinFung <gervinfungdaxuen@gmail.com>
2022-03-21 13:53:18 +0800

    test(ci-cd-room-population): removed logging

--- test/api/populate/room.ts
+++ test/api/populate/room.ts
@@ -185,8 +185,6 @@ const testRoomPopulate = () =>
                     )
                 ).rows;
 
-                console.dir(capacities, { depth: null });
-
                 expect(capacities.length).toBe(5);
 
                 expect(capacities[0]?.id).toBe(1);
